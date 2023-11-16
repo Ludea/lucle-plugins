73c3aec25a1ffbceb9417b84b4a13dd5f5fd2acc
Ludea <ludovicw35@hotmail.com>
2023-11-16 12:49:28 +0100

    Update lib.rs

--- smtp/src/lib.rs
+++ smtp/src/lib.rs
@@ -5,5 +5,9 @@ mod smtp_server;
 #[no_mangle]
 pub fn run() {
     let rt = tokio::runtime::Runtime::new().unwrap();
-    rt.block_on(smtp_server::start_smtp_server());
+
+    let join_handle = rt.spawn(async {
+        smtp_server::start_smtp_server().await;
+      });
+      rt.block_on(join_handle).unwrap();
 }
